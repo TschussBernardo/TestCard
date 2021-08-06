9b4f73059dc578b47a74e02c189b2d95dfd207da
TschussBernardo <86418593+TschussBernardo@users.noreply.github.com>
2021-08-06 16:36:23 +0200

    TestIntergartionUn Réalisation d'un ngFor sur une liste prédéfini dans le componentHero-form

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,35 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Hero }    from '../hero';
+import { Hero } from "../hero";
 
 @Component({
-  selector: 'app-hero-form',
-  templateUrl: './hero-form.component.html',
-  styleUrls: ['./hero-form.component.css']
+  selector: "app-hero-form",
+  templateUrl: "./hero-form.component.html",
+  styleUrls: ["./hero-form.component.css"]
 })
 export class HeroFormComponent {
+  powers = ["Really Smart", "Super Flexible", "Super Hot", "Weather Changer"];
 
-  powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
-
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  model = new Hero(18, "Dr IQ", this.powers[0], "Chuck Overstreet");
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  ensemble = ["super", "tu", "as", "gagne", "le", "droit", "de", "continuer"];
+
+  onSubmit() {
+    this.submitted = true;
+  }
 
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 
   newHero() {
-    this.model = new Hero(42, '', '');
+    this.model = new Hero(42, "", "");
   }
 
   skyDog(): Hero {
-    let myHero =  new Hero(42, 'SkyDog',
-                           'Fetch any object at any distance',
-                           'Leslie Rollover');
-    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    let myHero = new Hero(
+      42,
+      "SkyDog",
+      "Fetch any object at any distance",
+      "Leslie Rollover"
+    );
+    console.log("My hero is called " + myHero.name); // "My hero is called SkyDog"
     return myHero;
   }
 
@@ -38,10 +45,8 @@ export class HeroFormComponent {
   // Reveal in html:
   //   Name via form.controls = {{showFormControls(heroForm)}}
   showFormControls(form: any) {
-    return form && form.controls['name'] &&
-    form.controls['name'].value; // Dr. IQ
+    return form && form.controls["name"] && form.controls["name"].value; // Dr. IQ
   }
 
   /////////////////////////////
-
 }
